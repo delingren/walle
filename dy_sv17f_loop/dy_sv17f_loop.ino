6e091ac9f025c3268b5f3cf511d0ca404acb14a9
delingren <cookiejar.seattle@gmail.com>
2025-05-14 21:46:43 -0700

    Correct audio player test sketch

--- dy_sv17f_loop/dy_sv17f_loop.ino
+++ dy_sv17f_loop/dy_sv17f_loop.ino
@@ -6,14 +6,24 @@ DY::Player player(&Serial1);
 int file_count = 10;
 void setup() {
   player.begin();
-  delay(200);
+  Serial.begin(115200);
+  delay(400);
+  file_count = player.getSoundCount();
+  Serial.print("File count: ");
+  Serial.println(file_count);
   player.setVolume(20);
+  
 }
 
 void loop() {
-  static int index = 2;
+  static int index = 1;
   DY::play_state_t state = player.checkPlayState();
   if (state == DY::play_state_t::Stopped) {
+    Serial.print("Playing ");
+    Serial.print(index);
+    Serial.print("/");
+    Serial.println(file_count);
+
     player.playSpecified(index);
     index++;
     if (index > file_count) {
